5fab7f07db9ab296799372fc213244944815cc05
nschickle <nicholasschickle@gmail.com>
2020-12-06 14:31:22 -0500

    Implement Part 2

--- Dec3/main.cpp
+++ Dec3/main.cpp
@@ -18,38 +18,62 @@ int main()
     char passChar;
     int counter = 0;
     int currChar = 0;
+    int counter2 = 0;
+    int counter3 = 0;
     char c;
+    int posChar = 0;
+    int posChar2 = 0;
+    int count = 0;
 
     if (inputFile.is_open())
     {
+        int pwNum = 0;
         while (!inputFile.eof())
         {
+            pwNum++;
             inputFile >> minChar;
             inputFile >> c;
             inputFile >> maxChar;
             inputFile >> passChar;
             inputFile >> c;
             inputFile >> pw;
-            cout << minChar << " " << maxChar << " " << passChar << " " << pw << endl;
-
+            //cout << minChar << " " << maxChar << " " << passChar << " " << pw << endl;
+            posChar = minChar;
+            posChar2 = maxChar;
+            
+            
             for(string::iterator it=pw.begin();it!=pw.end(); ++it)
             {
+                counter2++;
                 if(*it == passChar)
                 {
-
                     currChar++;
-                    
+                    if(counter2 == posChar)
+                        count++;
+                    if(counter2 == posChar2)
+                        count++;
                 }
             }
-
+            
+            cout << pwNum << ": " << count << "\n";
+            if(count == 1)
+            {
+                counter3++;
+            }
             if(currChar >= minChar && currChar <= maxChar)
             {
-                cout << "Pass" << endl;
+                //cout << "Pass" << endl;
                 counter++;
             }
+            
+            
+            counter2 = 0;
+            count = 0;
             currChar = 0;          
         }
-        cout << counter;
+        cout << "Part 1: " << counter << " Valid Passwords\n";
+        cout << "Part 2: " << counter3 << " Valid Passwords\n";
+
     }
 
     inputFile.close();
